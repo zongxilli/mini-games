df6937fad8c60ca71a8832bc5e9be418419000c5
zongxi2014 <zongxi2014@gmail.com>
2024-05-24 23:53:46 -0400

    adjust tic tac toe styles

--- src/views/ticTacToe/components/components.styles.ts
+++ src/views/ticTacToe/components/components.styles.ts
@@ -1,6 +1,9 @@
 import { styled } from 'styled-components';
 
 export const BoardContainer = styled.div`
+  position: relative;
+  width: 20rem;
+  height: 20rem;
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   grid-template-rows: repeat(3, 1fr);
@@ -8,26 +11,35 @@ export const BoardContainer = styled.div`
 `;
 
 export const SquareContainer = styled.button`
-  height: 4rem;
-  width: 4rem;
+  height: 6rem;
+  width: 6rem;
   padding: 0.5rem;
   box-sizing: border-box;
   display: flex;
   align-items: center;
   justify-content: center;
   outline: 1px solid ${(props) => props.theme.colors.whiteDefault};
-  background: ${(props) => props.theme.colors.indigoDark};
+  background: ${(props) => props.theme.colors.indigoExtraLight};
   border-radius: 0.25rem;
   cursor: pointer;
 `;
 
 export const SquareText = styled.p`
-  font-size: 1rem;
+  font-size: 5rem;
 
   font-weight: 500;
-  line-height: 1.025rem;
   margin: 0;
   color: ${(props) => props.theme.colors.whiteDefault};
-  overflow: hidden;
   vertical-align: top;
+`;
+
+export const GameOverLabel = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
